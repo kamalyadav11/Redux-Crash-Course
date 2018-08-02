5f0bc47e9e20aa2436dc3776c1c24b2c82bd215f
kamal11 <yadav.kamal@thinkfuture.us>
2018-08-02 13:50:28 +0530

    added reducers in separate file, created actions folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './reducers/productsReducer';
+import userReducer from './reducers/userReducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const allreducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(
+  allreducers,
+  { //Setting Initial State
+    products: [{ name: 'iPhone' }],
+    user: 'Michael'
+  },
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
