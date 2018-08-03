9b0aba5dbbef1679642f47e12fed7e2e2075c764
kamal11 <kyzx11@gmail.com>
2018-08-03 19:12:27 +0530

    Added propTypes

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/postAction';
 
 class Posts extends Component {
@@ -21,6 +22,11 @@ class Posts extends Component {
       </div>
     )
   }
+}
+
+Posts.prototype = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
 }
 
 const mapStateToProps = state => ({
